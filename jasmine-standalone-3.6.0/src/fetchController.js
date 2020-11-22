3feb24ffda801e1d36d2299690087720a2770df8
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-11-22 13:20:36 +0000

    now you can create a new user, next step is to be able to login

--- jasmine-standalone-3.6.0/src/fetchController.js
+++ jasmine-standalone-3.6.0/src/fetchController.js
@@ -1,3 +1,10 @@
 window.onload = function() {
 fetchPeeps();
 }
+
+function getNewUser() {
+  var userName = document.getElementById ("username");
+  var password = document.getElementById ("password");
+  postUser(userName.value, password.value);
+  alert("Your Account was created, You can now Log in");
+}
