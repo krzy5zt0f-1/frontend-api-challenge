137ccac9b95fd2b601b9dd9628905f5ad42dc5dd
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-11-22 08:53:27 +0000

    finally have made the async await test for fetchPeeps() work :D

--- jasmine-standalone-3.6.0/spec/fetchSpec.js
+++ jasmine-standalone-3.6.0/spec/fetchSpec.js
@@ -1,18 +1,18 @@
 describe("fetchPeeps", function(){
   var promise;
-  beforeEach(function(){
-    spyOn(window.console, 'log');
+
+  it('fetches from the correct API', async function() {
+    spyOn(console, 'log');
     spyOn(window, 'fetch').and.callThrough();
-		promise = fetchPeeps();
-  });
-
-
-  it('fetches from the correct API', function() {
+		await fetchPeeps();
 		expect(window.fetch).toHaveBeenCalledWith('https://chitter-backend-api-v2.herokuapp.com/peeps');
+    expect(console.log).toHaveBeenCalledWith(jasmine.any(Array));
 	});
+  it("returns an array of objects that hodl inof about peeps, users etc.", async function(){
+    spyOn(console, 'log');
+    await fetchPeeps();
+    expect(console.log).toHaveBeenCalledWith(jasmine.any(Array));
+  })
 
-  it('returns a promise', function() {
-		expect(window.console.log).toHaveBeenCalled();
-	});
 
 })
--- jasmine-standalone-3.6.0/src/fetch.js
+++ jasmine-standalone-3.6.0/src/fetch.js
@@ -1,11 +1,21 @@
-function fetchPeeps() {
+async function fetchPeeps() {
   const url = 'https://chitter-backend-api-v2.herokuapp.com/peeps'
-  fetch(url)
+  await fetch(url)
   .then(function(resp) { return resp.json() }) // Convert data to json
   .then(function(data) {
     console.log(data);
+    displayPeeps(data);
   })
   .catch(function() {
     // catch any errors
   });
+  }
+
+  function displayPeeps( d ) {
+  	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+  	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32);
+
+  	document.getElementById('description').innerHTML = d.weather[0].description;
+  	document.getElementById('temp').innerHTML = celcius + '&deg;';
+  	document.getElementById('location').innerHTML = d.name;
   }
